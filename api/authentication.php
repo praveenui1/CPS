6202f1c98b28725757527515229d25ae749c8d2a
gopinath141 <gopinath141@users.noreply.github.com>
2017-12-03 21:04:01 -0600

    testuser

--- api/authentication.php
+++ api/authentication.php
@@ -2,6 +2,7 @@
 include "jsonFormat.php";
 include "Helpers/User.php";
 $user = new User;
+$testPassword = new tsetPassword;
 if(isset($_GET["username"]) && isset($_GET["password"])){
   echo json_encode($user -> authenticate($_GET["username"],$_GET["password"]), JSON_PRETTY_PRINT);
 } else if(isset($_POST["userName"]) && isset($_POST["passWord"])){
